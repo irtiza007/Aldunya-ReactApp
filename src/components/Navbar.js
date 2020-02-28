bb18b41631571ea78192c65ebe14920970d9eeab
irtiza007 <irtizahassan58@outlook.com>
2020-02-28 16:58:01 +0500

    logout added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,53 @@
-import React from "react";
-import { Assets } from "../config/Asset-Config";
-import NavItem from "./NavItem";
+import React from 'react';
+import { Assets } from '../config/Asset-Config';
+import NavItem from './NavItem';
+import { useHistory } from 'react-router-dom';
 
 const Navbar = () => {
+  const history = useHistory();
   return (
     <>
-      <nav className="navbar navbar-light navbar-horizontal  navbar-expand-lg shadow">
-        <div className="container">
-          <div className="avatar-img">
-            <img src={Assets.logo} alt="logo" />
+      <nav className='navbar navbar-light navbar-horizontal  navbar-expand-lg shadow'>
+        <div className='container'>
+          <div className='avatar-img'>
+            <img src={Assets.logo} alt='logo' />
           </div>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNav'
+            aria-controls='navbarNav'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <NavItem to="/" name="Clients" />
-              <NavItem to="/excercise" name="Excercise" />
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav ml-auto'>
+              <NavItem to='/' name='Clients' />
+              <NavItem to='/excercise' name='Excercise' />
               <li>
-                <div className="d-flex justify-content-center align-items-center ">
-                  <div className="avatar-img">
+                <div className='d-flex justify-content-center align-items-center '>
+                  <div
+                    className='avatar-img position-relative font-weight-bold'
+                    type='button'
+                  >
                     <img
                       src={Assets.userAvatar}
-                      alt="avatar"
-                      className="img-circle"
+                      alt='avatar'
+                      className='img-circle'
                     />
+                    <div
+                      type='button'
+                      className='text-custom-blue p-2 bg-white position-absolute logout'
+                      onClick={() => {
+                        localStorage.clear();
+                        history.push('/login');
+                      }}
+                    >
+                      <span>Logout</span>
+                    </div>
                   </div>
                 </div>
               </li>
