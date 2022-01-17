27c84525e951ffd8ee3b6df84967b1935f441958
irtiza.hassan <irtiza.hassan@carepvtltd.com>
2022-01-17 20:48:28 +0500

    minor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import axios from "axios";
 if (process.env.API) axios.defaults.baseURL = process.env.API;
-axios.defaults.baseURL = 'https://al-duyniya.herokuapp.com/';
+axios.defaults.baseURL = "https://physiotherapy-api.herokuapp.com/";
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
